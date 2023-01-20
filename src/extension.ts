a2cc79ebbf904c28ac1d8dcd45519be77f5211d5
Dennis Eriksson <dennis.eriksson@axis.com>
2023-01-20 17:39:16 +0100

    Add inital warning fix

--- src/extension.ts
+++ src/extension.ts
@@ -5,21 +5,41 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "vs-code-file-status" is now active!'
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vs-code-file-status" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let fileWarning = vscode.commands.registerCommand(
+    'vs-code-file-status.file-warning',
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vs-code-file-status.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vs-code-file-status!');
-	});
+      let diagnostics = vscode.languages.getDiagnostics();
+      const allWarnings = diagnostics[1][1].filter(
+        (r) => r.severity === vscode.DiagnosticSeverity.Warning
+      );
 
-	context.subscriptions.push(disposable);
+      allWarnings.forEach((w) => {
+        vscode.window.showInformationMessage(JSON.stringify(w.message));
+      });
+    }
+  );
+  context.subscriptions.push(fileWarning);
+
+  let fileError = vscode.commands.registerCommand(
+    'vs-code-file-status.file-error',
+    () => {
+      vscode.window.showInformationMessage('file error');
+    }
+  );
+
+  context.subscriptions.push(fileError);
 }
 
 // This method is called when your extension is deactivated
