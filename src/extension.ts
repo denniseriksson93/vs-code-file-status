0ca9a60cf5db4d7345ac9b4c0fb7bdc5d2c0eddc
Dennis Eriksson <dennis.eriksson@axis.com>
2023-02-03 12:49:45 +0100

    fix

--- src/extension.ts
+++ src/extension.ts
@@ -1,51 +1,45 @@
 import * as vscode from 'vscode';
 import { ListUtilities } from './list-utilities';
 
-export function activate(context: vscode.ExtensionContext) {
-  console.log(
-    'Congratulations, your extension "vs-code-file-status" is now active!'
+export const activate = (context: vscode.ExtensionContext) => {
+  context.subscriptions.push(
+    vscode.commands.registerCommand('vs-code-file-status.file-warning', () =>
+      jumpToFileStatus(vscode.DiagnosticSeverity.Warning)
+    )
   );
 
-  let fileWarning = vscode.commands.registerCommand(
-    'vs-code-file-status.file-warning',
-    () => {
-      let diagnostics = vscode.languages.getDiagnostics();
-      const allWarnings = diagnostics[1][1].filter(
-        (r) => r.severity === vscode.DiagnosticSeverity.Warning
-      );
-
-      const firstRowWithWarnings = ListUtilities.minBy(
-        allWarnings,
-        (p) => p.range.start.line
-      );
-
-      let editor = vscode.window.activeTextEditor;
-      if (editor) {
-        editor.selection = new vscode.Selection(
-          firstRowWithWarnings.range.start,
-          firstRowWithWarnings.range.end
-        );
-        editor.revealRange(firstRowWithWarnings.range);
-      }
-
-      allWarnings.forEach((w) => {
-        vscode.window.showInformationMessage(JSON.stringify(w.message));
-        vscode.window.showInformationMessage(JSON.stringify(w));
-        vscode.window.showInformationMessage('Alexandra är bäst');
-      });
-    }
+  context.subscriptions.push(
+    vscode.commands.registerCommand('vs-code-file-status.file-error', () =>
+      jumpToFileStatus(vscode.DiagnosticSeverity.Error)
+    )
   );
-  context.subscriptions.push(fileWarning);
+};
 
-  let fileError = vscode.commands.registerCommand(
-    'vs-code-file-status.file-error',
-    () => {
-      vscode.window.showInformationMessage('file error');
-    }
+const jumpToFileStatus = (severity: vscode.DiagnosticSeverity) => {
+  const textEditor = vscode.window.activeTextEditor;
+
+  if (!textEditor) {
+    return;
+  }
+
+  const allWithStatus = vscode.languages
+    .getDiagnostics()[1][1]
+    .filter((r) => r.severity === severity);
+
+  const firstRowWithStatus = ListUtilities.minBy(
+    allWithStatus,
+    (p) => p.range.start.line
   );
 
-  context.subscriptions.push(fileError);
-}
+  textEditor.selection = new vscode.Selection(
+    firstRowWithStatus.range.start,
+    firstRowWithStatus.range.end
+  );
+  textEditor.revealRange(firstRowWithStatus.range);
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+  allWithStatus.forEach((w) => {
+    vscode.window.showInformationMessage(JSON.stringify(w.message));
+  });
+};
+
+export const deactivate = () => {};
